bb539ad854c087a4f50f6e90f9ac44586b745c29
mlnck <jmelnick@jmelnick.com>
2017-10-04 18:33:44 -0400

    Progress on Server Routes

--- dist/commands/create-server-route.js
+++ dist/commands/create-server-route.js
@@ -1,11 +1,24 @@
 const chalk = require('chalk'),
   fs = require('fs'),
   sh = require('shelljs'),
+  { templateRename, verifyUniqueFile } = require('../utils'),
   basePath = process.env.PWD;
 
-function createServerRoute(obj)
+function createServerRoute(routeObj)
 {
-  console.log('createServerRoute: obj:', obj);
+  console.log('routeObj',routeObj);
+  const routeRoot = `${basePath}/server/routes`,
+    routeFile = `${routeRoot}/${routeObj.path.toLowerCase()}.routes.js`;
+  verifyUniqueFile(routeFile);
+
+  console.log(chalk.green.bgBlackBright.bold(' creating new server route: %s '), routeObj.path);
+
+  console.log(chalk.magenta('-- creating file'));
+  sh.cp(`${basePath}/config/templates/server/routes/_Structure.js`, `${routeFile}`);
+  templateRename(routeRoot, routeObj.path);
+
+  ///IF NOT CREATE CONTROLLER THEN GO INTO FILE AND REMOVE RELATED LINES
+  ///IF CREATE SCHEMA THEN RUN MLNCK-MERN SCHEMA ***
 }
 
 module.exports = createServerRoute;
